0ff1d277727b427ace8c2dfbd42e2e4fd6b21814
Shubham Shrotriya <87039505+shubhamshrotriya16427@users.noreply.github.com>
2023-10-08 01:40:42 -0700

    Time: 132 ms, Memory: 17.8 MB - LeetHub

--- move-zeroes/move-zeroes.py
+++ move-zeroes/move-zeroes.py
@@ -1,11 +1,13 @@
+
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        l=0
-        for r in range(len(nums)):
-           if nums[r]:
-                nums[l], nums[r]= nums[r], nums[l]
-                l=l+1
-        return nums
+        non_zero_index = 0
+
+        # Iterate through the list and move non-zero elements to the front.
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[non_zero_index] = nums[non_zero_index], nums[i]
+                non_zero_index += 1
+
+
+               
