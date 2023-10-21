290be25e2ad37570efbd1715fc771a4e7ecec935
Shubham Shrotriya <87039505+shubhamshrotriya16427@users.noreply.github.com>
2023-10-21 12:25:18 -0700

    Time: 194 ms, Memory: 20.7 MB - LeetHub

--- reverse-string/reverse-string.py
+++ reverse-string/reverse-string.py
@@ -3,6 +3,7 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
+        """
         stack = []
         
         for n in s:
@@ -11,3 +12,9 @@ class Solution:
         while stack:
             s[i] = stack.pop()
             i = i + 1
+        """
+        l, r = 0, len(s)-1
+        
+        while l<r:
+            s[l], s[r] = s[r], s[l]
+            l, r = l+1, r-1
