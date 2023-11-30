3475bac5bc4bec52ab89933943f3a77678cffbb8
Shubham Shrotriya <87039505+shubhamshrotriya16427@users.noreply.github.com>
2023-11-29 21:28:34 -0800

    Time: 484 ms, Memory: 31 MB - LeetHub

--- contains-duplicate/contains-duplicate.py
+++ contains-duplicate/contains-duplicate.py
@@ -5,7 +5,7 @@ class Solution:
         
         for i in nums:
             if i in hashset:
-               return True
+                return True
             else:
-              hashset.add(i)
+                hashset.add(i)
         return False
