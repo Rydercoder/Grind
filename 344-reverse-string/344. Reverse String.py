813786c82a39577e34478456c480e56a747f6265
Shubham Shrotriya <87039505+shubhamshrotriya16427@users.noreply.github.com>
2023-12-07 18:38:11 -0800

    Runtime: 200 ms (22.26%), Memory: 20.9 MB (17.70%) - Gitcode

--- 344-reverse-string/344. Reverse String.py
+++ 344-reverse-string/344. Reverse String.py
@@ -3,11 +3,18 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
+        """
         stack = []
         
         for n in s:
             stack.append(n)
         i=0
         while stack:
-             s[i] = stack.pop()
-             i= i+1
+            s[i] = stack.pop()
+            i = i + 1
+        """
+        l, r = 0, len(s)-1
+        
+        while l<r:
+            s[l], s[r] = s[r], s[l]
+            l, r = l+1, r-1
