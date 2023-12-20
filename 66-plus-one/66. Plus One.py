73b00b4b2d04fffeb9b792eda5fb2bc58263d686
Shubham Shrotriya <87039505+shubhamshrotriya16427@users.noreply.github.com>
2023-12-20 13:08:37 -0800

    Runtime: 36 ms (79.58%), Memory: 17.3 MB (18.05%) - Gitcode

--- 66-plus-one/66. Plus One.py
+++ 66-plus-one/66. Plus One.py
@@ -2,17 +2,17 @@ class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         
         digits = digits[::-1]
-        one,i = 1,0
+        one, i = 1, 0           
         
-        while one:
+        while one==1:
             if i<len(digits):
                 if digits[i]==9:
                     digits[i]=0
                 else:
                     digits[i]+=1
-                    one=0
+                    one = 0
             else:
                 digits.append(1)
-                one=0
+                one = 0
             i=i+1
         return digits[::-1]
