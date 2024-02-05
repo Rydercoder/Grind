b09567af3f0b8a7ba2b9b6bb3d8260e6c9326476
Shubham Shrotriya <87039505+shubhamshrotriya16427@users.noreply.github.com>
2024-02-05 15:20:37 -0800

    Time: 418 ms, Memory: 34.6 MB - LeetHub

--- contains-duplicate/contains-duplicate.py
+++ contains-duplicate/contains-duplicate.py
@@ -4,7 +4,7 @@ class Solution:
         hash_map = {}
         
         for i in nums:
-            if i in hash_map.keys():
+            if i in hash_map:
                 return True
             else:
                 hash_map[i] = 1
