1db0acdcc5a0f02a18824cef4b6b81e36c964cfd
Shubham Shrotriya <87039505+shubhamshrotriya16427@users.noreply.github.com>
2024-10-28 12:34:54 -0700

    Time: 15 ms, Memory: 17.6 MB - LeetHub

--- valid-anagram/valid-anagram.py
+++ valid-anagram/valid-anagram.py
@@ -1,21 +1,3 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        
-        
-       
-        
-        if len(s) != len(t):
-            return False
-        
-        hashmaS = {}
-        hashmaT = {}
-        
-        for i in range(len(s)):
-            hashmaS[s[i]] = 1 + hashmaS.get(s[i], 0)
-            hashmaT[t[i]] = 1 + hashmaT.get(t[i], 0)
-            
-        for c in hashmaS:
-            if hashmaS[c] != hashmaT.get(c, 0):
-                return False
-                
-        return True
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
