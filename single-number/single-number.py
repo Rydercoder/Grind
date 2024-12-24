8a2142241928d121b37f94f6d92f620374cbdb69
Shubham Shrotriya <87039505+shubhamshrotriya16427@users.noreply.github.com>
2024-12-23 17:06:12 -0800

    Time: 2 ms, Memory: 19.5 MB - LeetHub

--- single-number/single-number.py
+++ single-number/single-number.py
@@ -1,8 +1,7 @@
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int: 
-        res=0
-        for i in nums:
-            
-            res=res^i
-        return res
-                
+    def singleNumber(self, nums: List[int]) -> int:
+        
+        result = 0
+        for num in nums:
+            result ^= num
+        return result
